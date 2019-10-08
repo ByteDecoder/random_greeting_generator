a1ebe9e652d50b113574d2611946005ff11a33f9
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-10-07 23:55:34 -0500

    Initial commit

---
+++ lib/random_greeting_generator.rb
@@ -0,0 +1,6 @@
+require "random_greeting_generator/version"
+
+module RandomGreetingGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/random_greeting_generator/version.rb
@@ -0,0 +1,3 @@
+module RandomGreetingGenerator
+  VERSION = "0.1.0"
+end
